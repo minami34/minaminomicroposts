78fdd4b75c25f4d5232d494d4232233ea4efc7b3
jiro.kirameki <ec2-user@ip-172-31-37-27.us-east-2.compute.internal>
2018-06-04 04:33:04 +0000

    user registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);
     }
-
     /**
      * Register any application services.
      *
